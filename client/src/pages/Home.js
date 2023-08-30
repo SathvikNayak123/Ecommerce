561bc8e635e84a1c6b51ee61a4c64bf662d592ee
SathvikNayak123 <sathviknayak2@gmail.com>
2023-08-30 23:20:31 +0530

    Added my project

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import useFetch from '../hooks/useFetch.js';
+import LatestProducts from '../components/LatestProducts.js';
+import Hero from "../components/Hero.js";
+
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section>
+      <Hero/>
+      <LatestProducts/>
+    </section>
+  )
 };
 
 export default Home;
