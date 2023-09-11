323e8342136f866e82365fd1f5722b354efd1ef9
SathvikNayak123 <sathviknayak2@gmail.com>
2023-09-11 20:16:59 +0530

    fixed stripe code typo

--- api/client-strapi/src/api/order/controllers/order.js
+++ api/client-strapi/src/api/order/controllers/order.js
@@ -1,8 +1,7 @@
 //stripe
 const stripe=require('stripe')(process.env.STRIPE_KEY);
 
-
-
+('use strict');
 /**
  * order controller
  */
@@ -16,29 +15,31 @@ module.exports = createCoreController('api::order.order', ({strapi})=>({
       ctx.response.status=400;
       return {error: "Cart not found in request body"}
     }
-    const lineItems=await Promise.all(
-      cart.map(async (product)=>{
-        const item =await strapi.service("api::product.product").findOne(product.id);
-        return{
-          price_data:{
-            currency:"usd",
-            product_data:{
-              name:item.title,
-            },
-            unit_amount: item.price * 100,
-          },
-          quantity: product.amount,
-        }
-      })
-    )
     try{
+      const lineItems=await Promise.all(
+        cart.map(async (product)=>{
+          const item =await strapi.service("api::product.product").findOne(product.id);
+          return{
+            price_data:{
+              currency:"usd",
+              product_data:{
+                name:item.title,
+              },
+              unit_amount: Math.round(item.price * 100),
+            },
+            quantity: product.amount,
+          };
+        })
+      )
       const session=await stripe.checkout.sessions.create({
-        mode:"payment",
-        success_url: `${process.env.CLIENT_KEY}?success=true`,
-        cancel_url: `${process.env.CLIENT_KEY}?success=false`,
-        line_items: lineItems,
         shipping_address_collection: {allowed_countries: ["US", "CA"]},
         payment_method_types: ["card"],
+        mode:"payment",
+        // success_url: process.env.CLIENT_URL + '/success=true',
+        // cancel_url: process.env.CLIENT_URL + '/success=false'
+        success_url: `${process.env.CLIENT_URL}?success=true`,
+        cancel_url: `${process.env.CLIENT_URL}?success=false`,
+        line_items: lineItems,
       })
       await strapi.service("api::order.order").create({
         data: {
@@ -49,6 +50,7 @@ module.exports = createCoreController('api::order.order', ({strapi})=>({
       return { stripeSession: session}
     } catch(error){
       ctx.response.status=500;
+      return {error};
     }
   }
 }));
